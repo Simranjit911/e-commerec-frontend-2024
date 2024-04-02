dc81b1283a4b4184b9682467b2db88e79795f7fa
Simranjit Singh <simranjit94938@gmail.com>
2024-04-02 17:18:58 +0530

    Update axiosConfig.js 5

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 axios.defaults.withCredentials=true
 let instance = axios.create({
-    baseURL: "https://prokart-ecommerce-backend-2024.onrender.com"
-    // baseURL: "https://prokart-ecommerce-backend-2024.vercel.app"
+    // baseURL: "https://prokart-ecommerce-backend-2024.onrender.com"
+    baseURL: "https://prokart-ecommerce-backend-2024.vercel.app"
     // baseURL: "https://busy-rose-perch.cyclic.app"
 })
 export default instance
